62ced23f0bad2031cae65154c8518363410857e8
id1smq <id1smq@gmail.com>
2025-09-12 10:57:21 -0230

    Add files via upload

---
+++ Project2/story_content/triggers.js
@@ -0,0 +1,83 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "640F32JZ1XW":
+        Script1();
+        break;
+      case "6CIKpegyhec":
+        Script2();
+        break;
+      case "6pNeKlsHHGw":
+        Script3();
+        break;
+      case "6hNEviXaUji":
+        Script4();
+        break;
+      case "6mszTteBonn":
+        Script5();
+        break;
+      case "6lZFdZn37Aq":
+        Script6();
+        break;
+      case "6Q9uEdugUUZ":
+        Script7();
+        break;
+      case "5WS5Em88FMH":
+        Script8();
+        break;
+      case "6YheqStCipd":
+        Script9();
+        break;
+      case "6cvY6iGgVa9":
+        Script10();
+        break;
+      case "5WgsVmYmOyo":
+        Script11();
+        break;
+      case "5uxPPK9KEP3":
+        Script12();
+        break;
+      case "6LlxvDnqhss":
+        Script13();
+        break;
+      case "5sGpDlHN38V":
+        Script14();
+        break;
+      case "5iA5LEPbCPB":
+        Script15();
+        break;
+      case "5whahqbRIwx":
+        Script16();
+        break;
+      case "68ygNHpk3qb":
+        Script17();
+        break;
+      case "6Pi02L39dx3":
+        Script18();
+        break;
+      case "5Z5svDJnKtk":
+        Script19();
+        break;
+      case "6b3HGBD2NpG":
+        Script20();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
