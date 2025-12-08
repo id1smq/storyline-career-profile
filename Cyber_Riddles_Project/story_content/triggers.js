d9a5a52f825a0d5e54fda80d91826fd82bab88e2
id1smq <id1smq@gmail.com>
2025-12-08 13:37:37 -0330

    Add files via upload

--- Cyber_Riddles_Project/story_content/triggers.js
+++ Cyber_Riddles_Project/story_content/triggers.js
@@ -2,59 +2,62 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6POpAejflSh":
+      case "6CQH2MNRv1j":
         Script1();
         break;
-      case "5XMJAn0OBW8":
+      case "61qaYJkaCET":
         Script2();
         break;
-      case "5X4FVCk8SKW":
+      case "6gz1WfWOEl9":
         Script3();
         break;
-      case "6ntv3EwMOuy":
+      case "61DoWBKXoKg":
         Script4();
         break;
-      case "5jWMLzhbpSR":
+      case "5bFX3gclcsf":
         Script5();
         break;
-      case "6HfHVubL5jg":
+      case "5jGrVFIvIqN":
         Script6();
         break;
-      case "5mBfHtV0Ldl":
+      case "6r7bYshrAx4":
         Script7();
         break;
-      case "6VR78JqNiDm":
+      case "6DyVggDH6D7":
         Script8();
         break;
-      case "6algtqYmqHp":
+      case "5wlXeEDIqMb":
         Script9();
         break;
-      case "68JBhhxV9gU":
+      case "6iQIxgS78k2":
         Script10();
         break;
-      case "6V4czrS9qwd":
+      case "6GWfcREoGup":
         Script11();
         break;
-      case "6k43Nojnowb":
+      case "6Ns41Duq57T":
         Script12();
         break;
-      case "6ga8IS1Zm3Z":
+      case "5fsmags9Bhx":
         Script13();
         break;
-      case "6h8IvVMvGF9":
+      case "5cBtvYpYMuP":
         Script14();
         break;
-      case "6GfN0goeYBz":
+      case "5ku2Jllj6bC":
         Script15();
         break;
-      case "5siQFiyPBPX":
+      case "5g2fr7tmWhq":
         Script16();
         break;
-      case "5s0CP6f6Z46":
+      case "64gn1ugxTVF":
         Script17();
         break;
-      case "6gYXy80NEOB":
+      case "6JomsX9Ntwh":
         Script18();
+        break;
+      case "5lBzWpK17Dj":
+        Script19();
         break;
   }
 }
--- Cyber_Riddles_Project/story_content/user.js
+++ Cyber_Riddles_Project/story_content/user.js
@@ -671,10 +671,75 @@ loadJSfile(scriptFolder+"JavaScript/xapianonlearner.js", "js");
 
 window.Script12 = function()
 {
-  manageTimer.course.stop()
+  var loadedCount = 0; //keep track of how many libraries loaded, so we do not reload the same again
+var amountOfLibs = 2; //How many unique JS libraries are we loading
+var player = GetPlayer();
+
+//Place WO with empty index.html and all external file sets in individual folders
+//on stand alone slide in another scene. Set to open into a new window.
+//Add a button to that slide that relaunches the slide with the WO.
+//publish that slide, click button, and record the location of the WO folder
+//which is text string afte "/story_content/WebObjects/" in URL
+//Save string in woFolder variable so this script can find and load files
+var woFolder = player.GetVar("woFolder"); //Get location of the WO files in the SL site
+console.log("woFolder: " + woFolder);
+
+function loadJSfile(filename, filetype){
+	if (filetype=="js"){ //if filename is a external JavaScript file
+		var fileref=document.createElement('script');
+		
+		fileref.setAttribute("type","text/javascript");
+		fileref.setAttribute("src", filename);
+
+		fileref.onload = function() {
+			loadedCount++;
+			console.log(loadedCount+" JS / "+filename+' loaded.');
+
+			if(loadedCount >= amountOfLibs){//if we loaded all the unique JS files, then indicate and exit
+			//register the plugin (just once)
+				//gsap.registerPlugin(MotionPathPlugin);
+				
+				//will only run this loader when javascriptsLoaded == false
+				player.SetVar("javascriptsLoaded", true);
+			}
+		};
+	}
+	else if (filetype=="css"){ //if filename is an external CSS file
+		var fileref=document.createElement("link")
+
+		fileref.setAttribute("rel", "stylesheet")
+		fileref.setAttribute("type", "text/css")
+		fileref.setAttribute("href", filename);
+
+		fileref.onload = function() {
+			loadedCount++;
+			console.log(loadedCount+" CSS / "+filename+' loaded.');
+
+			if(loadedCount >= amountOfLibs){
+				player.SetVar("javascriptsLoaded", true);
+			}
+		};
+	}
+	if (typeof fileref!="undefined") {
+		document.getElementsByTagName("head")[0].appendChild(fileref)
+	}
+}
+
+//When we change anything in the scriptFolder this needs to change
+var scriptFolder ="story_content/WebObjects/" + woFolder + "/";
+
+//And lastly we call the functions we want to run
+//add the folder names and files names to the WO path
+loadJSfile(scriptFolder+"JavaScript/xapiwrapper.min.js", "js");
+loadJSfile(scriptFolder+"JavaScript/xapianonlearner.js", "js");
 }
 
 window.Script13 = function()
+{
+  manageTimer.course.stop()
+}
+
+window.Script14 = function()
 {
   var loadedCount = 0; //keep track of how many libraries loaded, so we do not reload the same again
 var amountOfLibs = 2; //How many unique JS libraries are we loading
@@ -739,30 +804,30 @@ loadJSfile(scriptFolder+"JavaScript/xapiwrapper.min.js", "js");
 loadJSfile(scriptFolder+"JavaScript/xapianonlearner.js", "js");
 }
 
-window.Script14 = function()
+window.Script15 = function()
 {
   sendStatement("course")
 
 }
 
-window.Script15 = function()
+window.Script16 = function()
 {
   manageTimer.slide.reset();
 }
 
-window.Script16 = function()
+window.Script17 = function()
 {
   populateLeaderboard()
 }
 
-window.Script17 = function()
+window.Script18 = function()
 {
   populateleaderboard();
 
 
 }
 
-window.Script18 = function()
+window.Script19 = function()
 {
   boldLeaderboard();
 }
